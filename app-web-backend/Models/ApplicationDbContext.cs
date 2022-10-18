42349d239b653258cfaa202c6829926cbb12a6c8
carolina <carolinavlaurindo@gmail.com>
2022-10-18 20:41:18 -0300

    Adicionar arquivos de projeto.

---
+++ app-web-backend/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace app_web_backend.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext(DbSet<Usuario>? usuarios)
+        {
+            Usuarios = usuarios;
+        }
+
+        public DbSet<Veiculos>? Veiculos { get; set; }
+        public DbSet<Consumo>?  Consumos { get; set; }
+        public DbSet<Usuario>? Usuarios { get; set; }
+    }
+}
---
+++ app-web-backend/Models/Veiculos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app_web_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculos
+    {
+        [Key]
+        public int  Id{ get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
+        public string? Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar a placa")]
+        public string? Placa { get; set; }
+
+        public ICollection<Consumo>? Consumos  { get; set; }
+    }
+}
